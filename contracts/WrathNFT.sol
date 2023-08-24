fa551eb2ec29611e85f06f5628dafc7c0a5aadb7
Raphael Nembhard <raphaelnembhard@Raphaels-MBP.fios-router.home>
2023-08-24 19:38:09 -0400

    Changed TotalSupply

--- contracts/WrathNFT.sol
+++ contracts/WrathNFT.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.13;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract WrathNFT is ERC721 {
-    uint16 public immutable MAX_SUPPLY = 10000;
+    uint16 public immutable MAX_SUPPLY = 100;
     uint256 public immutable MINT_PRICE = 0.5 ether;
 
     uint16 public currentSupply = 0;
